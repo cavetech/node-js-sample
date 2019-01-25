4044f24adaab44d87d241ed80ea30b46e3126016
Huber <tkjh1@c02xf2zfjg5j.unigroupinc.com>
2019-01-25 15:56:04 -0600

    Fixes

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ Sentry.init({ dsn: 'https://485af430310941b68a340198c51b52c6@sentry.io/1380006' 
 var express = require('express')
 var app = express()
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 3000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
   //response.send('Hello World!')
-  Sentry.captureMessage(request)
+  Sentry.captureMessage(response)
 })
 
 app.listen(app.get('port'), function() {
